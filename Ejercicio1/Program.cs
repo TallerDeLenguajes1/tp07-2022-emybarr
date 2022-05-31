f8d69864f517ca51dc594cd76c72f29b3b75f76d
emybarr <eybarreiro.2a@gmail.com>
2022-05-31 16:19:34 -0300

    avan

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace Ejercicio1
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+           List<tareas> listaTareasPendientes = new List<tareas>();
+           List<tareas> listaTareasRealizadas = new List<tareas>();
+
+
+
+
+
+
+}
+}
+}
--- Ejercicio1/tareas.cs
+++ Ejercicio1/tareas.cs
@@ -7,6 +7,26 @@ namespace Ejercicio1
 {
     public class tareas
     {
-        
+        private int tareaId;
+
+        private string descripcion;
+
+        private int duracion;
+
+        public int TareaId { get => tareaId; set => tareaId = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public int Duracion { get => duracion; set => duracion = value; }
+
+
+
+        public static tareas creartarea(){
+            tareas nuevatarea = new tareas();
+            Random rand = new Random();
+            Console.WriteLine("Ingrese Descripcion ");
+            nuevatarea.Descripcion = Console.ReadLine();
+            nuevatarea.Duracion = rand.Next(1,12);
+            return nuevatarea;
+
+        }
     }
 }
